bee349f4c933b584685e2fae37afe617497b6eed
michlz <zmichal.zieba@gmail.com>
2026-04-14 20:31:57 +0200

    Implement refresh and logout functionality with JWT support in auth service

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter,Depends
+from fastapi import APIRouter,Depends,HTTPException,Response,Cookie
 from app.database import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.schemas.auth import RegisterRequest, LoginRequest,LoginResponse
-from app.services.auth_service import register_user,login_user
+from app.services.auth_service import register_user,login_user,logout,refresh_access_token
 router = APIRouter()
 
 
@@ -15,7 +15,38 @@ async def register(request: RegisterRequest, db: AsyncSession = Depends(get_db))
 
 
 @router.post("/login", response_model= LoginResponse)
-async def login(request: LoginRequest, db: AsyncSession = Depends(get_db)):
+async def login(request: LoginRequest,response: Response, db: AsyncSession = Depends(get_db)):
     
     result = await login_user(db,request.email,request.password)
+    response.set_cookie(
+        key="refresh_token",
+        value=result["refresh_token"],
+        httponly=True,
+        secure=True,
+        samesite="lax"
+    )
+
+    return LoginResponse(
+        access_token=result["access_token"],
+        token_type="bearer",
+        salt=result["salt"]
+    )
+
+
+@router.post("/refresh")
+async def refresh(refresh_token: str = Cookie(None), db: AsyncSession = Depends(get_db)):
+    if refresh_token is None:
+        raise HTTPException(status_code=401,detail="No refresh token")
+    
+    return await refresh_access_token(db,refresh_token)
+
+
+
+@router.post("/logout")
+async def logout_endpoint(response: Response, refresh_token: str = Cookie(None),db: AsyncSession = Depends(get_db)):
+    if refresh_token is None:
+        raise HTTPException(status_code=401,detail="No refresh token")
+    
+    result = await logout(db,refresh_token)
+    response.delete_cookie("refresh_token")
     return result
--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -1,12 +1,13 @@
 from argon2 import PasswordHasher
-from sqlalchemy import select
+from sqlalchemy import select,delete
 import logging
 from jose import jwt
 from datetime import datetime,timedelta
 from fastapi import HTTPException
-from app.models.models import User
+from app.models.models import User,RefreshToken
 from app.config import settings
 from argon2.exceptions import VerifyMismatchError
+import secrets
 logger = logging.getLogger(__name__)
 ph = PasswordHasher(time_cost = settings.ARGON2_TIME_COST,
                     memory_cost= settings.ARGON2_MEMORY_COST)
@@ -59,8 +60,21 @@ async def login_user(db,email,password):
             "exp" : datetime.now() + timedelta(minutes=15)
         }
         jwt_token = jwt.encode(payload,settings.JWT_SECRET,algorithm="HS256")
+        refresh_token = secrets.token_hex(32)
 
-        return {"access_token" : jwt_token, "salt" : user.salt}
+        refresh_token_record = RefreshToken(
+            user_id = user.id,
+            token= refresh_token,
+            expires_at = datetime.now() + timedelta(days=7)
+        )
+
+        db.add(refresh_token_record)
+        await db.commit()
+
+
+        return {"access_token" : jwt_token,
+                "refresh_token": refresh_token, 
+                "salt" : user.salt}
     
     except VerifyMismatchError:
         raise HTTPException(status_code=401,detail="Invalid credentials")
@@ -68,3 +82,43 @@ async def login_user(db,email,password):
         logger.error("Verification error: %s",e)
         raise HTTPException(status_code=500, detail="Internal server error")
 
+
+async def refresh_access_token(db,token):
+
+    result = await db.execute(select(RefreshToken).where(RefreshToken.token == token))
+
+    token_db = result.scalar_one_or_none()
+
+    if token_db is None:
+        logger.error("RefreshToken not found")
+        raise HTTPException(status_code=401,detail="Token Error")
+    
+    if token_db.expires_at < datetime.now():
+        raise HTTPException(status_code=401,detail="Refresh token expired")
+
+
+
+    payload = {
+        "sub": str(token_db.user_id),
+        "exp": datetime.now() + timedelta(minutes=15)
+    }
+    new_jwt = jwt.encode(payload,settings.JWT_SECRET,algorithm="HS256")
+
+    return {"access_token" : new_jwt}
+    
+
+async def logout(db, token):
+    
+    result = await db.execute(select(RefreshToken).where(RefreshToken.token ==token)) 
+
+    token_db = result.scalar_one_or_none()
+
+    if token_db is None:
+        logger.error("RefreshToken not found")
+        raise HTTPException(status_code=401,detail="Token Error")
+    
+
+    await db.delete(token_db)
+    await db.commit()
+
+    return {"message" : "logged out"}
