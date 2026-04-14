ae6d6861610446e26b350891139b449509a68481
michlz <zmichal.zieba@gmail.com>
2026-04-14 19:52:58 +0200

    Implement user registration and login endpoints with FastAPI

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,3 +1,21 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter,Depends
+from app.database import get_db
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.schemas.auth import RegisterRequest, LoginRequest,LoginResponse
+from app.services.auth_service import register_user,login_user
 router = APIRouter()
+
+
+@router.post("/register")
+async def register(request: RegisterRequest, db: AsyncSession = Depends(get_db)):
+
+    await register_user(db,request.email,request.password,request.salt)
+
+    return {"message" : "registered successfully"}
+
+
+@router.post("/login", response_model= LoginResponse)
+async def login(request: LoginRequest, db: AsyncSession = Depends(get_db)):
+    
+    result = await login_user(db,request.email,request.password)
+    return result
